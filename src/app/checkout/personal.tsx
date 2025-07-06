c773361e6798654d2f196d35a8d026f159691e31
TranML <the44secrets@gmail.com>
2025-07-06 11:20:33 +0700

    handle-edge-cases: personal: add safe area view

--- src/app/checkout/personal.tsx
+++ src/app/checkout/personal.tsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, ScrollView } from "react-native";
 import CustomButton from "../../components/CustomButton";
 import { router } from "expo-router";
 import CustomTextInput from "../../components/CustomTextInput";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function PersonalDetailsFormScreen() {
   const onNext = () => {
@@ -15,85 +16,37 @@ export default function PersonalDetailsFormScreen() {
     <ScrollView
       style={{ backgroundColor: "white" }}
       contentContainerStyle={styles.container}
+      // keyboardShouldPersistTaps="handled"
     >
-      <CustomTextInput label="Full Name" placeholder="John Doe" />
+      <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+        <CustomTextInput label="Full Name" placeholder="John Doe" />
 
-      <CustomTextInput
-        label="Address"
-        placeholder="123 Main St, Anytown, USA"
-      />
-      <View style={{ flexDirection: "row", gap: 8 }}>
         <CustomTextInput
-          label="City"
-          placeholder="Anytown"
-          containerStyle={{ flex: 1 }}
+          label="Address"
+          placeholder="123 Main St, Anytown, USA"
         />
+        <View style={{ flexDirection: "row", gap: 8 }}>
+          <CustomTextInput
+            label="City"
+            placeholder="Anytown"
+            containerStyle={{ flex: 1 }}
+          />
+          <CustomTextInput
+            label="Postal Code"
+            placeholder="12345"
+            inputMode="numeric"
+            containerStyle={{ flex: 1 }}
+          />
+        </View>
+
         <CustomTextInput
-          label="Postal Code"
-          placeholder="12345"
-          inputMode="numeric"
-          containerStyle={{ flex: 1 }}
+          label="Phone Number"
+          placeholder="1234567890"
+          inputMode="tel"
         />
-      </View>
 
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-      <CustomTextInput
-        label="Phone Number"
-        placeholder="1234567890"
-        inputMode="tel"
-      />
-
-      <CustomButton title="Next" onPress={onNext} style={styles.button} />
+        <CustomButton title="Next" onPress={onNext} style={styles.button} />
+      </SafeAreaView>
     </ScrollView>
   );
 }
