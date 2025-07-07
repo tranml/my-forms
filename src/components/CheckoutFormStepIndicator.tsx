18ed0ec4e6911ecbfff1ded04876ac93e529fa5e
TranML <the44secrets@gmail.com>
2025-07-07 14:48:16 +0700

    refactor: improve CheckoutFormStepIndicator with dynamic step rendering and better styling

--- src/components/CheckoutFormStepIndicator.tsx
+++ src/components/CheckoutFormStepIndicator.tsx
@@ -1,12 +1,29 @@
 import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const steps = [
+  {
+    key: "personal",
+    title: "Pesonnal",
+  },
+  {
+    key: "payment",
+    title: "Payment",
+  },
+  {
+    key: "confirm",
+    title: "Confirm",
+  },
+];
+
 export default function CheckoutFormStepIndicator() {
   return (
     <SafeAreaView edges={["top"]} style={styles.container}>
-      <Text>Pesonnal</Text>
-      <Text>Payment</Text>
-      <Text>Confirm</Text>
+      {steps.map((step) => (
+        <View key={step.key} style={styles.stepContainer}>
+          <Text style={styles.stepTitle}>{step.title}</Text>
+        </View>
+      ))}
     </SafeAreaView>
   );
 }
@@ -18,5 +35,16 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "lightblue",
     height: 110,
+    gap: 16,
+  },
+  stepContainer: {
+    flex: 1,
+    borderBottomWidth: 3,
+    borderBottomColor: "red",
+  },
+  stepTitle: {
+    textAlign: "center",
+    fontSize: 12,
+    fontWeight: "bold",
   },
 });
