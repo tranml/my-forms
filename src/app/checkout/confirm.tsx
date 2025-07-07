06e1cd2b16f5cd7e8ffe8c3b70c28e514a3e553a
TranML <the44secrets@gmail.com>
2025-07-07 10:25:52 +0700

    confirm: add dummy personal & payment info

--- src/app/checkout/confirm.tsx
+++ src/app/checkout/confirm.tsx
@@ -3,6 +3,21 @@ import CustomButton from "../../components/CustomButton";
 import { router } from "expo-router";
 import KeyboardAwareScrollView from "../../components/KeyboardAwareScrollView";
 
+const personalInfo = {
+  fullName: "TranML",
+  address: "123 27th St",
+  city: "Khanh Hoa",
+  postcode: "59000",
+  phone: "0909090909",
+  country: "VN",
+};
+
+const paymentInfo = {
+  cardNumber: "1234123412341234",
+  expires: "01/30",
+  cvv: "123",
+};
+
 export default function ConfirmationFormScreen() {
   const onSubmit = () => {
     //validate form
