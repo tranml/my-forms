3c6574b4b520e3cdc6009c392607c73b04932bca
TranML <the44secrets@gmail.com>
2025-07-07 11:23:36 +0700

    refactor:confirm: remove hardcoded data and use checkout form context in confirm screen

--- src/app/checkout/confirm.tsx
+++ src/app/checkout/confirm.tsx
@@ -2,23 +2,11 @@ import { Text, StyleSheet, View } from "react-native";
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
 import KeyboardAwareScrollView from "../../components/KeyboardAwareScrollView";
-
-const personalInfo = {
-  fullName: "TranML",
-  address: "123 27th St",
-  city: "Khanh Hoa",
-  postcode: "59000",
-  phone: "0909090909",
-  country: "VN",
-};
-
-const paymentInfo = {
-  cardNumber: "1234123412341234",
-  expires: "01/30",
-  cvv: "123",
-};
+import { useCheckoutForm } from "../../contexts/CheckoutFormProvider";
 
 export default function ConfirmationFormScreen() {
+  const { personalInfo, paymentInfo } = useCheckoutForm();
+
   const onSubmit = () => {
     //validate form
 
